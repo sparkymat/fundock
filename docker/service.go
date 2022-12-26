f86368007e13200ae19b73c9becc33fba3333848
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2022-12-26 23:26:50 +1100

    Added image pull

--- docker/service.go
+++ docker/service.go
@@ -3,7 +3,9 @@ package docker
 import (
 	"context"
 	"fmt"
+	"io"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	dockerlib "github.com/docker/docker/client"
@@ -24,17 +26,48 @@ type Service struct {
 }
 
 func (s *Service) Run(ctx context.Context, image string, input string) (string, error) {
-	containerCfg := &container.Config{}
-	hostConfig := &container.HostConfig{}
+
+	err := s.pullImage(ctx, image)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.createContainer(ctx, image, input)
+
+	return "", err
+}
+
+func (s *Service) pullImage(ctx context.Context, image string) error {
+	resp, err := s.client.ImagePull(ctx, image, types.ImagePullOptions{
+		All: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = io.ReadAll(resp)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (s *Service) createContainer(ctx context.Context, image string, input string) error {
+	containerCfg := &container.Config{
+		Image: image}
+	hostConfig := &container.HostConfig{
+		AutoRemove: true,
+	}
 	nwConfig := &network.NetworkingConfig{}
 	pf := &specs.Platform{}
 
-	body, err := s.client.ContainerCreate(ctx, containerCfg, hostConfig, nwConfig, pf, image)
+	body, err := s.client.ContainerCreate(ctx, containerCfg, hostConfig, nwConfig, pf, "")
 	if err != nil {
-		return "", fmt.Errorf("failed to create container. err: %w", err)
+		return fmt.Errorf("failed to create container. err: %w", err)
 	}
 
 	fmt.Printf("id=%v\n", body.ID)
 
-	return "", nil
+	return nil
 }
